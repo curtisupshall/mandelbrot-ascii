b44aa066cbba7633b4cb8b6fafbd943ffb22131e
Curtis Upshall <curtisupshall@gmail.com>
2021-02-08 01:30:22 -0800

    Remove comments

--- main.c
+++ main.c
@@ -9,11 +9,7 @@ struct Complex {
     double i;
 };
 
-struct Complex origin = {.r = (double)0, .i = (double)0};
-
-void printZ (struct Complex z) {
-    printf("(%.3f %c%.3fi)", z.r, z.i < 0 ? '-' : '+', fabs(z.i));
-}
+struct Complex origin = { .r = 0.0, .i = 0.0};
 
 struct Complex square (struct Complex z) {
     struct Complex zSquared = {
@@ -21,12 +17,6 @@ struct Complex square (struct Complex z) {
         .i = 2 * z.r * z.i
     };
 
-/*
-    printZ(z);
-    printf(" squared = ");
-    printZ(zSquared);
-    printf("\n");
-*/
     return zSquared;
 }
 
@@ -44,46 +34,17 @@ struct Complex f_z (struct Complex z, struct Complex c) {
 }
 
 double distance (struct Complex z1, struct Complex z2) {
-    double dist = sqrt(pow(z1.r - z2.r, 2) + pow(z1.i - z2.i, 2));
-    /*
-    printf("distance(");
-    printZ(z1);
-    printf(", ");
-    printZ(z2);
-    printf(") = %.3f\n", dist);
-    */
-    return dist;
+    return sqrt(pow(z1.r - z2.r, 2) + pow(z1.i - z2.i, 2));
 }
 
-/**
- * Quanitifies the stability of a complex constant
- * @param iteration The current iteration
- * @param c The complex constant
- * @param threshold The threshold where, if the distance between f(c) and the origin is less than this, f(c) is considered stable
- */
 double calculateStability (int iteration, struct Complex z, struct Complex c, double threshold) {
-    // Check if c is stable.
-
-    /*
-    printf("z = ");
-    printZ(z);
-    printf("\n");
-
-    */
     if (distance(z, origin) > 2) {
-        // printf("distance(z, origin) > 2.\n");
         return 0;
     }
     struct Complex zn =  f_z(z, c);
     if (distance(z, zn) < threshold || iteration > 200) {
-        // printf("z is stable for iteration %d\n", iteration);
         return (double)1 / iteration;
     } else {
-        /*
-        printf("Checking stability for ");
-        printZ(zn);
-        printf("\n");
-        */
         return calculateStability(iteration + 1, zn, c, threshold);
     }
 }
@@ -95,6 +56,7 @@ int main () {
     double scale = 1;
     double pScale = 1;
     double re = -2.0;
+
     double pRe = -2.0;
     double im = 1;
     double pIm = 1;
@@ -108,14 +70,10 @@ int main () {
             for (int k = 0; k < width; k ++) {
                 c.r = re + (double)k * scale * (double)3 / (RES * 9/4);
                 c.i = im - (double)j * scale * (double)3 / RES;
-                /*
-                printZ(c);
-                printf("\n");
-                */
-                stability = calculateStability(1, f_z(origin, c), c, 0.1);
-                // printf("Stability: %.3f\n", s);
 
-                char shade = stability > 0 ? '%' : '.';
+                stability = calculateStability(1, f_z(origin, c), c, 0.1);
+
+                char shade = stability > 0 ? '%' : ' ';
                 printf("%c", shade);
                 
             }
@@ -124,7 +82,9 @@ int main () {
         scanf("%d", &n);
         printf("\n");
         
-        if (n <= 0) {
+        if (n < 0)
+            return 0;
+        else if (n == 0) {
             scale = pScale;
             re = pRe;
             im = pIm;
@@ -144,8 +104,6 @@ int main () {
                 re += scale * (n - 1);
             }
         }
-        
-       // printf("n = %d", n);
     }
 
     return 0;
